1f1cc022073813da41044ef600304956ddcf03ef
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-03-25 00:06:25 +0900

    updated carrierwave setting.

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,12 +2,12 @@ unless Rails.env.development? || Rails.env.test?
   CarrierWave.configure do |config|
     config.fog_credentials = {
       provider: 'AWS',
-      aws_access_key_id: 'AKIAWPKMISLS43WRYOVS',
-      aws_secret_access_key: '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',
+      aws_access_key_id: 'AKIAWPKMISLSS5AVDHHP',
+      aws_secret_access_key: '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',
       region: 'ap-northeast-1'
     }
 
-    config.fog_directory  = 'the-power-of-dreams'
+    config.fog_directory  = 'kidsvillage'
     config.cache_storage = :fog
   end
 end
