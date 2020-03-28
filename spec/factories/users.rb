be851a7112104749693d98194e81926bb4e37f48
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-03-28 10:53:21 +0900

    updated image uploader

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     password = Faker::Internet.password(8)
     name { Faker::Name.name }
     email { Faker::Internet.free_email }
+    image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/support/test.jpg')) }
     password { password }
     password_confirmation { password }
   end
