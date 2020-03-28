8756cb2e248933c622c11ade6ba40d6a8ab945db
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-03-28 14:25:10 +0900

    updated factories/user.rb

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :user do
-    password = Faker::Internet.password(8)
     name { Faker::Name.name }
     email { Faker::Internet.free_email }
     image { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/support/test.jpg')) }
