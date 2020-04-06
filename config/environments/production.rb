3d13c35d700b993ffb3562a122e159aeb8dcce27
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-04-06 22:54:42 +0900

    Revised action mailer config.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,8 +66,6 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'whispering-sierra-93462.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
