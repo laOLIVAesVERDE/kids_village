23132507430068e6f898be3df3aeb4670d7ea452
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-04-27 12:33:30 +0900

    Deleted about action/view.

--- spec/requests/access_to_static_pages_spec.rb
+++ spec/requests/access_to_static_pages_spec.rb
@@ -22,15 +22,6 @@ RSpec.describe 'Access to static_pages', type: :request do
     end
   end
 
-  context 'GET #about' do
-    before { get about_path }
-    it 'responds successfully' do
-      expect(response).to have_http_status 200
-    end
-    it "has title 'About | Ruby on Rails Sample App'" do
-      expect(response.body).to include full_title('About')
-    end
-  end
 
   context 'GET #contact' do
     before { get contact_url }
