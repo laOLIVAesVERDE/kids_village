e4791de9fe27be36d4e8cde025fdee4b6ed13b0b
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-06-20 18:46:43 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,12 +5,13 @@ if Rails.env.production?
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
       provider: 'AWS',
-      region:  ENV['S3_REGION'],
-      aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
-      aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY']
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
+    
       # aws_access_key_id: 'AKIAWPKMISLSSPMOVRXL',
       # aws_secret_access_key: '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',
-      
+
     }
   end
 end
