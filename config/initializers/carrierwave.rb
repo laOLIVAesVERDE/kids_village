d1c4191fc7c2d2fdc59af0baf79e643614d606ea
laOLIVAesVERDE <57529705+laOLIVAesVERDE@users.noreply.github.com>
2020-06-20 18:48:54 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,6 +1,6 @@
 if Rails.env.production?
   CarrierWave.configure do |config|
-    config.fog_directory  = 'kidsvillage'
+    config.fog_directory  = ENV['S3_BUCKET']
     config.cache_storage = :fog
     config.fog_provider = 'fog/aws'
     config.fog_credentials = {
@@ -8,10 +8,6 @@ if Rails.env.production?
       :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
       :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
       :aws_secret_access_key => ENV['S3_SECRET_KEY']
-    
-      # aws_access_key_id: 'AKIAWPKMISLSSPMOVRXL',
-      # aws_secret_access_key: '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',
-
     }
   end
 end
